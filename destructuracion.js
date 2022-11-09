f0e1a7cbfa7788e0d9e7ae7810df7d18d27ad5d4
Julia Macias <julyemacias@gmail.com>
2022-11-09 18:11:20 -0500

    primera practica

--- destructuracion.js
+++ destructuracion.js
@@ -20,7 +20,6 @@ console.log(nombre);
 function printFullName ({nombre,apellido}){
     console.log(nombre,apellido);
 }
-
 printFullName(usuario);
 const {nombre:name} = usuario;
 console.log(name);

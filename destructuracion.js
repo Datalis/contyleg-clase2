d23ba630c596a71177fa4175ace0b89f4e4d23c0
Julia Macias <julyemacias@gmail.com>
2022-11-09 18:10:10 -0500

    primera practica

---
+++ destructuracion.js
@@ -0,0 +1,26 @@
+const usuario = {
+    nombre:"Julia",
+    apellido:"macias",
+    edad:"20",
+    ciudad:"guayaquil"
+}
+
+function printName(){
+    return {
+        nombre:"Julia",
+        apellido:"macias",
+        edad:"20",
+        ciudad:"guayaquil"
+    }
+}
+
+const  {nombre} = printName();
+console.log(nombre);
+
+function printFullName ({nombre,apellido}){
+    console.log(nombre,apellido);
+}
+
+printFullName(usuario);
+const {nombre:name} = usuario;
+console.log(name);

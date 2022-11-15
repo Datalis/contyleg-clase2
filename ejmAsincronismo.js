d4159c6cbaea6dae8c8d4f30d0ece6b2609e4144
Antonio <avillegas@cyl.com.ec>
2022-11-15 15:17:18 -0500

    subir cambio modificacion ejemplo

--- ejmAsincronismo.js
+++ ejmAsincronismo.js
@@ -5,28 +5,28 @@ async function pedidoDoc(){
     try {
 
         const responseDcto = await axios.post('https://viatics.xoait.com/validate-physical-invoice', {
-                                                "emissionDate": "2022-08-02",
-                                                "printingAuth": "1130292611",
-                                                // "ruc": "1801193531001",
-                                                "externDocTypeId": "01",
-                                                "docNum": "001-001-000012501"
-                                            })
-        if(!responseDcto.ok){
-            throw new Error('Http error: ${responseDcto.status}');
+                                                    "emissionDate": "2022-08-02",
+                                                    "printingAuth": "1130292611",
+                                                    "ruc": "1801193531001",
+                                                    "externDocTypeId": "01",
+                                                    "docNum": "001-001-000012501"
+                                                })
+        if(responseDcto.statusText !== 'OK'){
+            throw new Error('Http error', responseDcto.status);
         }
 
          //console.log(responseDcto);
          console.log(responseDcto.data); 
-         console.log('Received response: ${responseDcto.status}');
-                                                                                
-
+         console.log('Received response:',responseDcto.status);
+       
 
     }catch(error){
-        console.log('Error al obtener Documento: ${error}');   //Esta toma los errores que estan arriba y la general
+        console.log('Error al obtener Documento:', error);   
     }
 }
 
-pedidoDocDoc();
+pedidoDoc();
 
+console.log('Ejecutando...')
 
       

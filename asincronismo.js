858aeb5ee27eb51480799f98c9b9b6e6b44bd62e
CESAR MADRID <cmadrid@DOM-ASC-GYE.LOCAL>
2022-11-18 10:35:04 -0500

    Prueba async test

--- asincronismo.js
+++ asincronismo.js
@@ -2,21 +2,36 @@ const axios = require('axios');
 
 //Promesas
 
-const promise = axios.post('https://viatics.xoait.com/validate-physical-invoice', {
-    "emissionDate": "2022-08-02",
-    "printingAuth": "1130292611",
-    "ruc": "1801193531001",
-    "externDocTypeId": "01",
-    "docNum": "001-001-000012501"
-})
+// const promise = axios.post('https://viatics.xoait.com/validate-physical-invoice', {
+//     "emissionDate": "2022-08-02",
+//     "printingAuth": "1130292611",
+//     "ruc": "1801193531001",
+//     "externDocTypeId": "01",
+//     "docNum": "001-001-000012501"
+// })
 
-promise.then( res =>{
-    console.log(res.data)
-}).catch( e => {
-    console.log(e)
-})
+// promise.then( res =>{
+//     console.log(res.data)
+// }).catch( e => {
+//     console.log(e)
+// })
+
+//Funcion Aync await
+async function test(){
+    const data = await axios.post('https://viatics.xoait.com/validate-physical-invoice', {
+        "emissionDate": "2022-08-02",
+        "printingAuth": "1130292611",
+        "ruc": "1801193531001",
+        "externDocTypeId": "01",
+        "docNum": "001-001-000012501"
+    })
+    
+    console.log(data)
+}
 
 console.log("Ejecutando...")
+
+test();
 
 
 
